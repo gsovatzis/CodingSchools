cae897bb56d395260059683c67a9b608c1fdc7d0
George Sovatzis <gsovatzis@hotmail.com>
2020-11-14 17:09:06 +0200

    Added a DEMO program for SingletonExample

---
+++ SingletonExample/SingletonExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SingletonExample
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+
+         DBManager dBManager = DBManager.instance;
+         dBManager.getConnection();
+      }
+   }
+}
