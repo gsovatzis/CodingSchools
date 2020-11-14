df322133d6c46852c596e141f1a9daec46c928f7
George Sovatzis <gsovatzis@hotmail.com>
2020-11-14 16:55:54 +0200

    Pushed a main Program for FactoryExample DEMO

---
+++ FactoryExample/FactoryExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryExample
+{
+   public static class Program
+   {
+      public static void Main(string[] args)
+      {
+         Console.WriteLine("Hello");
+
+         DBFactory dBFactory = new DBFactory("mssql");
+         IDatabase<Product> dbHandler = dBFactory.GetDatabase<Product>();
+         Product p = new Product();
+
+         dbHandler.Add(p);
+
+      }
+   }
+}
